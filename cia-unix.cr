59a347bc63bbf024c5b304d2321d9975f99eb3e8
Niakr1s <pavel2188@gmail.com>
2026-06-10 02:31:11 +0300

    removed debug print

--- cia-unix.cr
+++ cia-unix.cr
@@ -82,7 +82,7 @@ Dir["*.3ds"].each do |ds|
         end
         args += ["-i", "#{ncch}:#{i}:#{i}"]
     end
-    puts "Building decrypted #{dsn} 3DS..., running makerom with args: #{args}"
+    puts "Building decrypted #{dsn} 3DS..."
     run_tool("makerom", args)
     check_decrypt(dsn, "3ds")
     remove_cache
